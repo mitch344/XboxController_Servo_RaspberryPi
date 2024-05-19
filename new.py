fa9035db7bdb462c0e921e885b75a2f5d02c429e
mitch344 <35354833+mitch344@users.noreply.github.com>
2024-05-19 09:18:41 -0700

    Update new.py

--- new.py
+++ new.py
@@ -30,8 +30,6 @@ try:
         if event.type == evdev.ecodes.EV_KEY:
             if event.code == evdev.ecodes.BTN_A and event.value == 1:  # A button pressed
                 move_servo(0)  # Move to 0 degrees
-            elif event.code == evdev.ecodes.BTN_B and event.value == 1:  # B button pressed
-                move_servo(90)  # Move to 90 degrees
             elif event.code == evdev.ecodes.BTN_X and event.value == 1:  # X button pressed
                 move_servo(180)  # Move to 180 degrees
             else:
